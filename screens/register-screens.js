17c98782f48ce19e4aa551ea7cb5dc86922c3e83
Noja, Iverson <iversonnoja@gmail.com>
2023-04-16 20:16:30 +0800

    Register

--- screens/register-screens.js
+++ screens/register-screens.js
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const RegisterScreen = () => {
     return (
         <View style={styles.screen}>
-            <Text>This is Register</Text>
+            <View style={styles.input}>
+                <TextInput style={styles.input} placeholder="First Name"/>
+                <TextInput style={styles.input} placeholder="Last Name"/>
+                <TextInput style={styles.input} placeholder="Middle Initial"/>
+                <TextInput style={styles.input} placeholder="Address"/>
+                <TextInput style={styles.input} placeholder="Password"/>
+                <TextInput style={styles.input} placeholder="Email"/>
+            </View>
+
         </View>
     );
 }
