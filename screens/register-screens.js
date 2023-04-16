96b1580094cd0e83060a60b997807edf876acf00
Noja, Iverson <iversonnoja@gmail.com>
2023-04-16 21:45:45 +0800

    Centered placement

--- screens/register-screens.js
+++ screens/register-screens.js
@@ -5,11 +5,6 @@ const RegisterScreen = () => {
         <View style={styles.screen}>
             <View style={styles.input}>
                 <TextInput style={styles.input} placeholder="First Name"/>
-                <TextInput style={styles.input} placeholder="Last Name"/>
-                <TextInput style={styles.input} placeholder="Middle Initial"/>
-                <TextInput style={styles.input} placeholder="Address"/>
-                <TextInput style={styles.input} placeholder="Password"/>
-                <TextInput style={styles.input} placeholder="Email"/>
             </View>
 
         </View>
